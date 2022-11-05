0e6dc160aaaff52a0750dd523410f83aaed84a0c
João Gabriel <joao.25.ribeiro01@gmail.com>
2022-11-05 17:03:47 -0300

    Quarta parte e efeitos em js

---
+++ reveal.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.primeiraImagem', { duration: 1000 });
+
+sr.reveal('.primeiraTexto3', { duration: 1000 });
+
+sr.reveal('.segundaConteudo', { duration: 1000 });
+
+sr.reveal('.segundaImagem', { duration: 1000 });
+
+sr.reveal('.terceiraConteudo', { duration: 1000 });
+
+sr.reveal('.mapa', { duration: 1000 });
+
+sr.reveal('.quartaConteudo', { duration: 1000 });
